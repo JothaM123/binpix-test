2a365fd7079eb17525afe557cfbb68d16aa3c344
Jotham <jothamsamuel05@gmail.com>
2022-07-21 20:32:43 +0530

    Update binpix.js

--- binpix.js
+++ binpix.js
@@ -1,12 +1,12 @@
-function pix(element, art, theme, color) { 
+function pix(element, art, theme, color, width, height) { 
 
         var resultd = art.replace(/0/g, "\xa0".repeat(3))
         var finald = resultd.replace(/1/g, "█")
         var el = document.getElementById(element)
         el.style.background= theme
         el.innerHTML = finald
-        el.style.width = "max-content"
-        el.clientHeight.width = "max-content"
+        el.style.width = width
+        el.style.height = height
         el.style.paddingBottom = "20px"
         el.style.color = color
    
